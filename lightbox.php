c26e602a786ac023574faad45292cedf80eb5f41
angegevela <202280079@psu.palawan.edu.ph>
2024-11-11 20:47:20 +0800

    It is working or no errors is seen in the project. I am also satisfied on this project as of now, if i am given more time i will expand it more to my liking and more ideas that i have now.

--- lightbox.php
+++ lightbox.php
@@ -234,7 +234,7 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
-}
+}//Javascript Code
 </script>
 </body>
 </html>
